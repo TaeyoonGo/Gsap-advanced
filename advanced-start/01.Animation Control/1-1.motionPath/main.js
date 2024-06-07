7fa84f39134805d5c4b780fab7f0599d0519d12d
gotaeyoon <ty.go.payple.kr>
2024-06-07 18:02:08 +0900

    Main.js 완료

--- advanced-start/01.Animation Control/1-1.motionPath/main.js
+++ advanced-start/01.Animation Control/1-1.motionPath/main.js
@@ -6,21 +6,46 @@ const $ = (node) => document.querySelector(node);
 
 
 const tiger = $('#tiger')
+const BUTTON = $('#button')
+const time = $('#time')
+const progress = $('#progressSlider')
 
-const animation = gsap.to(tiger,{
-    duration:6,
-    motionPath:{
-        path:"#route",
-        align:tiger
-    }
+
+
+const animation = gsap.to(tiger, {
+    duration: 6,
+    motionPath: {
+        path: "#route",
+        align: tiger
+    },
+    onUpdate: update,
+    onComplete: () => BUTTON.textContent = 'play'
 })
 
-animation.timeScale(3);
+function update(){
+    time.textContent = animation.time().toFixed(2);
+}
 
 
-console.log(animation.timeScale(3))
+BUTTON.addEventListener('click', () => {
+    animation.paused(!animation.paused());
+    if (animation.progress() === 1) {
+        animation.restart()
+    }
+    BUTTON.textContent = animation.paused() ? 'play' : 'pause'
+})
 
 
+progress.addEventListener('input',() => {
+
+})
+
+//문 vs 식
+//값을 반환하지 않음 / 값을 반환
+
+//조건 문 / if / switch / 반복 문 / for / while / do..while
+
+// 삼항 연산자 : 계산되는 피 연산자가 3개
 
 
-
+// const result  = condition ? value1 : value2
