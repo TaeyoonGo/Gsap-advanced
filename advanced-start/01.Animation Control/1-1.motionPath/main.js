e58bea2064387dd7983311756413b503655c7c3e
gotaeyoon <ty.go.payple.kr>
2024-06-11 14:24:46 +0900

    GSAP 복습 진행중

--- advanced-start/01.Animation Control/1-1.motionPath/main.js
+++ advanced-start/01.Animation Control/1-1.motionPath/main.js
@@ -13,7 +13,7 @@ const home = $('#home')
 const mountain = $('#mountain')
 const river = $('#river')
 const company = $('#company')
-
+const svg = $('svg')
 
 const animation = gsap.to(tiger, {
     duration: 6,
@@ -25,22 +25,25 @@ const animation = gsap.to(tiger, {
     onComplete: () => BUTTON.textContent = 'play'
 })
 
-function update(){
+function update() {
     progress.value = animation.progress();
     time.textContent = animation.time().toFixed(2);
 }
 
+function setButtonState(){
+    BUTTON.textContent = animation.paused() ? 'play' : 'pause'
+}
 
 BUTTON.addEventListener('click', () => {
     animation.paused(!animation.paused());
     if (animation.progress() === 1) {
         animation.restart()
     }
-    BUTTON.textContent = animation.paused() ? 'play' : 'pause'
+    setButtonState();
 })
 
 
-progress.addEventListener('input',(e) => {
+progress.addEventListener('input', (e) => {
 
     let target = e.currentTarget
     BUTTON.textContent = 'play'
@@ -49,50 +52,76 @@ progress.addEventListener('input',(e) => {
 
 
 
-home.addEventListener('click',function(){
+
+svg.addEventListener('click', (e) => {
+    const target = e.target.closest('g');
+    const id = target.getAttribute('id');
+    let progress = 0;
+
+    if (!target) return;
+    if (!id) return;
+    if(id === 'svg') return;
+
+
     animation.pause();
-    gsap.to(animation,{
-        progress: 0,
-        duration:3
-    })
+    switch (id) {
+        case 'home' :
+            progress = 0;
+            break;
+        case 'mountain' :
+            progress = 0.238;
+            break;
+        case 'river' :
+            progress = 0.47;
+            break;
+        case 'company' :
+            progress = 1
+            break;
+    }
+
+    if(id === 'svg'){
+        return;
+    }
+
+
+
+    gsap.to(animation, {progress: progress, duration: 1})
+    setButtonState();
+
 })
 
-mountain.addEventListener('click',function(){
-    animation.pause();
-    gsap.to(animation,{
-        progress: 0.238,
-        duration:3
-    })
-})
-
-river.addEventListener('click',function(){
-    animation.pause();
-    gsap.to(animation,{
-        progress: 0.47,
-        duration:3
-    })
-})
-
-
-company.addEventListener('click',function(){
-    animation.pause();
-    gsap.to(animation,{
-        progress: 1,
-        duration:3
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
+// home.addEventListener('click',function(){
+//     animation.pause();
+//     gsap.to(animation,{
+//         progress: 0,
+//         duration:3
+//     })
+// })
+//
+// mountain.addEventListener('click',function(){
+//     animation.pause();
+//     gsap.to(animation,{
+//         progress: 0.238,
+//         duration:3
+//     })
+// })
+//
+// river.addEventListener('click',function(){
+//     animation.pause();
+//     gsap.to(animation,{
+//         progress: 0.47,
+//         duration:3
+//     })
+// })
+//
+//
+// company.addEventListener('click',function(){
+//     animation.pause();
+//     gsap.to(animation,{
+//         progress: 1,
+//         duration:3
+//     })
+// })
 
 
 //문 vs 식
